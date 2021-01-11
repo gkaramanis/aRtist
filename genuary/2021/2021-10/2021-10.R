64608c28ec2765ba3cdf16041cf9230c2076bfbf
Georgios <georgios.karamanis@me.com>
2021-01-11 11:16:16 +0100

    Add day 10

---
+++ genuary/2021/2021-10/2021-10.R
@@ -0,0 +1,16 @@
+library(treemapify)
+library(ggplot2)
+
+tree <- data.frame(n = 1:1900, v = runif(1900, 0, 4000), g = round(runif(1900, 1, 16)))
+
+col1 <- "#e1dd72"
+col2 <- "#1b6535"
+
+ggplot(tree) +
+  geom_treemap(aes(area = v, fill = v-n, subgroup = g), color = "grey10", start = "bottomright") +
+  scale_fill_gradient(low = col2, high = col1) +
+  coord_fixed(expand = FALSE) +
+  theme(
+    legend.position = "none"
+        ) +
+  ggsave(here::here("genuary", "2021", "2021-10", "2021-10.png"), dpi = 320, width = 7, height = 7)
---
+++ genuary/2021/2021-10/2021-10b.R
@@ -0,0 +1,22 @@
+library(ape)
+library(ggtree)
+library(ggplot2)
+
+trees <- lapply(rep(100, 9), rtree, rooted = TRUE)
+class(trees) <- "multiPhylo"
+
+# ggplot(tree) +
+#   geom_tree(aes(ladderize=TRUE))
+
+ggtree(trees, ladderize = TRUE, branch.length = "none", layout = "circular",
+       size = 0.3, color = "#1e2761") +
+  geom_tippoint(color = "#FDAC4F", size = 0.7) +
+  # geom_highlight(node = round(runif(100, 1, 100)), fill = "purple", size = 1) +
+  facet_wrap(vars(.id)) +
+  theme_void() +
+  theme(
+    strip.text = element_blank(),
+    plot.background = element_rect(fill = "grey97", color = NA)
+  ) +
+  ggsave(here::here("genuary", "2021", "2021-10", "2021-10b.png"), dpi = 320, width = 7, height = 7)
+
