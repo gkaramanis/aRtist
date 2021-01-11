2a277fb1c2506671f510e072e4c75720a8447dd3
Georgios Karamanis <georgios@karaman.is>
2021-01-11 19:51:56 +0100

    Fix day again

--- genuary/2021/2021-11/2021-11.R
+++ genuary/2021/2021-11/2021-11.R
@@ -3,14 +3,14 @@ library(dplyr)
 library(ggplot2)
 library(grid)
 
-# file_wav <- here::here("genuary", "2021", "2021-13", "data", "Beethoven_Piano_Concerto_4_slow_movement,_bars_47-55.wav")
+file_wav <- here::here("genuary", "2021", "2021-11", "data", "Beethoven_Piano_Concerto_4_slow_movement,_bars_47-55.wav")
 
-file_wav <- here::here("genuary", "2021", "2021-13", "data", "Beethoven,_Piano_Sonata,_Op_111,_first_movement_concluding_bars.wav")
+# file_wav <- here::here("genuary", "2021", "2021-11", "data", "Beethoven,_Piano_Sonata,_Op_111,_first_movement_concluding_bars.wav")
 
 wav <- readWave(file_wav)
 
 wav_df <- data.frame(left = wav@left, right = wav@right) %>% 
-  head(1000) %>%
+  head(1500) %>%
   mutate(
     n = row_number(),
     g = n %/% (wav@samp.rate / 1000)
@@ -29,7 +29,8 @@ p <- ggplot(wav_df, aes(right, left)) +
     axis.title.y = element_text(angle = 0, vjust = 0.5)
   )
 
+png(file="genuary/2021/2021-11/2021-11b.png", width = 1200, height = 1200)
 grid.newpage()
 pushViewport(viewport(name = "rotate", angle = 45, width = 0.75, height = 0.75))
 print(p, vp = "rotate")
-
+dev.off()
