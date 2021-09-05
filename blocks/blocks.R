5295bb2064de6c15af65e88e59b23bf92f2072d5
Georgios Karamanis <georgios.karamanis@me.com>
2021-09-05 18:57:35 +0200

    undo mistake

--- blocks/blocks.R
+++ blocks/blocks.R
@@ -1,21 +1,37 @@
-library(magick)
 library(tidyverse)
-library(grid)
+library(ggfx)
+library(ggimage)
+library(magick)
 
 url <- "https://images.unsplash.com/flagged/photo-1556743749-bb5642435a63?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
 
 img <- image_read(url)
+h = image_info(img)$height
+w = image_info(img)$width
 
-h = 100
-w = 100
+blocks <- data.frame(n = 1:60) %>% 
+  rowwise() %>% 
+  mutate(
+    x = runif(1, 0, w),
+    y = runif(1, 0, h),
+    s = runif(1, 40, 100)
+  )
 
-img_lst <- lapply(seq_along(1:30), function(i){
-  x = runif(1, 0, image_info(img)$width)
-  y = runif(1, 0, image_info(img)$height)
-  image_crop(image_background(img, "purple", flatten = FALSE), paste0(h ,"x", w, "+", x, "+", y), repage = FALSE)
-  })
-  
-blocks <- Reduce(`c`, img_lst) %>%
-  image_mosaic()
+ggplot() +
+  geom_image(aes(x = w/2, y = h/2, image = url), size = 1, asp = w/h) +
+  as_reference(
+    geom_tile(data = blocks, aes(x, y, width = s, height = s), fill = "black"),
+    id = "blocks"
+  ) +
+  with_shadow(
+    with_blend(
+      geom_image(aes(x = w/2, y = h/2, image = url), size = 1.2, asp = w/h),
+      bg_layer = "blocks",
+      blend_type = "in"
+    ),
+    sigma = 5
+    ) +
+  coord_fixed(xlim = c(0, w), ylim = c(0, h)) +
+  theme_void()
 
-blocks
+ggsave(here::here("blocks", "plots", "blocks.png"), dpi = 320, width = w, height = h, units = "px")
